e1ffd5b4be791ac6ba8014bfb7beff1dd3e89953
manish774 <manishranbir774@gmail@example.com>
2024-04-13 11:30:50 +0000

    Auto-generated files

--- src/services/ticketsNew.ts
+++ src/services/ticketsNew.ts
@@ -1,6 +1,42 @@
 import { fetchUsersProps } from "./ticketsNewModel";
+import { fetchUsersNewProps } from "./ticketsNewModel";
+import { fetchUsers2Props } from "./ticketsNewModel";
+import { fetchUsers1Props } from "./ticketsNewModel";
 
 export const fetchUsers = async (props: fetchUsersProps) => {
+  const fetchData = await fetch(
+    `https://api.github.com/users/${props?.name}/$${props?.userId}`,
+    {
+      method: "POST",
+      body: JSON.stringify(props),
+    },
+  );
+  const data = await fetchData.json();
+  return data;
+};
+
+export const fetchUsersNew = async (props: fetchUsersNewProps) => {
+  const fetchData = await fetch(`https://api.github.com/users/user`, {
+    method: "POST",
+    body: JSON.stringify(props),
+  });
+  const data = await fetchData.json();
+  return data;
+};
+
+export const fetchUsers2 = async (props: fetchUsers2Props) => {
+  const fetchData = await fetch(
+    `https://api.github.com/users/${props?.name}/$${props?.userId}`,
+    {
+      method: "POST",
+      body: JSON.stringify(props),
+    },
+  );
+  const data = await fetchData.json();
+  return data;
+};
+
+export const fetchUsers1 = async (props: fetchUsers1Props) => {
   const fetchData = await fetch(
     `https://api.github.com/users/${props?.name}/$${props?.userId}`,
     {
