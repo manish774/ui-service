9673f69b256fcc31534a4776d3831ca8010915d4
manish774 <manishranbir774@gmail.com>
2024-04-17 17:31:56 +0000

    Changes pushed by automated service: Auto-generated files

---
+++ src/services/NewTestModel.ts
@@ -0,0 +1,18 @@
+export interface fetchUsersProps {
+  name: string;
+  userId: string;
+}
+
+export interface fetchUsersNewProps {
+  name: string;
+}
+
+export interface fetchUsers2Props {
+  name: string;
+  userId: string;
+}
+
+export interface fetchUsers1Props {
+  name: string;
+  userId: string;
+}
